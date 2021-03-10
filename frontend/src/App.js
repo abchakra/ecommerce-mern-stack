29568a982616e01bbcff2b55c5baf6a9ef33d9d0
chakraborty <chakraborty@silexica.com>
2021-03-10 20:19:34 +0100

    Setup bootstrap fontawesome navbar footer and header css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
+
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Petshop</h1>
+        </Container>
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
