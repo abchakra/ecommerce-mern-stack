197a4c920ece4e64cede46360dafa095016cb91f
abchakra <abhishek.chakraborty26@gmail.com>
2021-03-11 13:16:28 +0100

    Add Product Details Reducer and Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer } from './reducers/produceReducers'
+import { productDetailsReducer, productListReducer } from './reducers/produceReducers'
 
 const reducer = combineReducers({
 
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 })
 
 const initialState = {}
